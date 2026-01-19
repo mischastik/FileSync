b3826a07e9e498151b8d2a7275f68f6c55fa15fa
Michael Balda <balda@metrilus.de>
2026-01-19 15:24:06 +0100

    More console logging

--- FileSync.Server/Network/TcpServer.cs
+++ FileSync.Server/Network/TcpServer.cs
@@ -143,7 +143,7 @@ public class TcpServer
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error handling client: {ex.Message}");
+                Console.WriteLine($"Error handling client: {ex.Message}\n{ex.StackTrace}");
             }
         }
     }
